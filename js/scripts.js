730d3dada059d1d38a433ccc5cf007f2345fcc32
MemoLee <softwarewewo@gmail.com>
2023-01-08 22:26:27 +0300

    Js update

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(window).scroll( function() {   
+        var sticky = $('#home_header'),
+        scroll = $(window).scrollTop();
+        if (scroll >= 200) {
+            sticky.removeClass('home-header');
+            sticky.addClass('home-header2');
+        } else {
+            sticky.addClass('home-header');
+            sticky.removeClass('home-header2');
+        }
+    });
+});
